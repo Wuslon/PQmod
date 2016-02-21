3452fcbb26515e1f3787c7c029f3dc4e8fa2119d
Christian Preuss <christian.preusz@yahoo.de>
2016-02-21 21:20:20 +0100

    added some comments and changed formation
    Signed-off-by: Christian Preuss <christian.preusz@yahoo.de>

--- pqmod.pl
+++ pqmod.pl
@@ -12,66 +12,81 @@ $pq_modified =~ s/\.gpx$/\_solved\.gpx/;
 $flag = 0;
 $ctr = 0;
 
-#Read solution file
+#Read solution file and put it in array (name, latitude, longitude)
 open($fh, "<$solution_file") or die "Can't open file: $!";
 while(<$fh>){
-	my $line = $_;
-	my @fields = split " ", $line;
-	for (my $i = 0; $i<3; $i++){
-		$solution[$ctr][$i] = $fields[$i];
-	}
-	$ctr++;
+    my $line = $_;
+    my @fields = split " ", $line;
+    for (my $i = 0; $i<3; $i++){
+        $solution[$ctr][$i] = $fields[$i];
+    }
+    $ctr++;
 }
 close $fh;
 
-#Umrechnung der Loesungen
+#conversion of ccordinates to gpx mode
 for(0..$#solution){
-	$solution[$_][1] =~ s/N//;
-	$solution[$_][1] =~ s/S/-/;
-	$solution[$_][1] =~ /(\d*\.\d*)/;
-	my $modified = $1*1000000/60;
-	$modified =~ s/\..*//;
-	$solution[$_][1] =~ s/(\d*\.\d*)/$modified/;
-	$solution[$_][1] =~ s/°/./;
-	
-	$solution[$_][2] =~ s/E//;
-	$solution[$_][2] =~ s/W/-/;
-	$solution[$_][2] =~ /(\d*\.\d*)/;
-	$modified = $1*1000000/60;
-	$modified =~ s/\..*//;
-	$solution[$_][2] =~ s/(\d*\.\d*)/$modified/;
-	$solution[$_][2] =~ s/°/./;
+    #latitude
+    $solution[$_][1] =~ s/N//;
+    $solution[$_][1] =~ s/S/-/;
+    $solution[$_][1] =~ /(\d*\.\d*)/;
+    my $modified = $1*1000000/60;
+    $modified =~ s/\..*//;
+    $solution[$_][1] =~ s/(\d*\.\d*)/$modified/;
+    $solution[$_][1] =~ s/°/./;
+    #longitude
+    $solution[$_][2] =~ s/E//;
+    $solution[$_][2] =~ s/W/-/;
+    $solution[$_][2] =~ /(\d*\.\d*)/;
+    $modified = $1*1000000/60;
+    $modified =~ s/\..*//;
+    $solution[$_][2] =~ s/(\d*\.\d*)/$modified/;
+    $solution[$_][2] =~ s/°/./;
 }
 
+#read original pocket query and replace coordinates
 open($fh, "<$pq_original") or die "Can't open file: $!";
 while(<$fh>){
-	my $line = $_;
-	$flag = 1 if($line =~ /<wpt/);
-	if ($flag == 0){
-		push(@output,$line);
-	}
-	else{
-		push(@temp_out,$line);
-	}
-	if($line =~ /^\s*<name>/){
-		my $temp = $line;
-		$temp =~ s/^\s*<name>//;
-		$temp =~ s/\s*<\/name>\s*$//;
-		for(0..$#solution){
-			#print "Solution: $solution[$_][0]\ntemp: $temp\n";
-			if ($solution[$_][0] eq $temp){
-				$temp_out[0] =~ s/lat="\d*\.\d*"/lat="$solution[$_][1]"/;
-				$temp_out[0] =~ s/lon="\d*\.\d*"/lon="$solution[$_][2]"/;
-				last;
-			}
-		}
-		push(@output,@temp_out);
-		@temp_out = ();
-		$flag = 0;
-	}
+    my $line = $_;
+    #search for tag wpt
+    $flag = 1 if($line =~ /<wpt/);
+    if ($flag == 0){
+        #store line in output array
+        push(@output,$line);
+    }
+    else{
+        #after wpt store lines in another array
+        push(@temp_out,$line);
+    }
+    if($line =~ /^\s*<name>/){
+        #filter the name
+        my $temp = $line;
+        $temp =~ s/^\s*<name>//;
+        $temp =~ s/\s*<\/name>\s*$//;
+        for(0..$#solution){
+            
+            #search for the same name in solution file and replace
+            #coordinates in first entry of temp_out
+            if ($solution[$_][0] eq $temp){
+                $temp_out[0] =~ s/lat="\d*\.\d*"/lat="$solution[$_][1]"/;
+                $temp_out[0] =~ s/lon="\d*\.\d*"/lon="$solution[$_][2]"/;
+                last;
+            }
+        }
+        
+        #push second array to output array
+        push(@output,@temp_out);
+        
+        #flush temp_out
+        @temp_out = ();
+        
+        #write next lines to output array
+        $flag = 0;
+    }
 }
 close $fh;
 
+#store everything in the new file
 open($fh, ">$pq_modified") or die "Can't create file: $!";
 print $fh @output;
 close $fh;
